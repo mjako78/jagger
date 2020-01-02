0196ce77fa7084324990f8b71ff0da2f7abed0c1
Mauro Giacomini <mauro.giaco@gmail.com>
2020-01-02 22:00:11 +0100

    Add sample method for demo purpose only

---
+++ src/jagger.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <jagger/jagger.h>
+
+void hello() {
+  printf("Hello World!");
+  printf("From JAGGER!");
+}
--- tests/testlib.cpp
+++ tests/testlib.cpp
@@ -1,14 +1,19 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
+#include "jagger/jagger.h"
 
 int factorial(int number) {
   return number > 1 ? factorial(number - 1) * number : 1;
 }
 
-TEST_CASE("testing the factorial method") {
+TEST_CASE("Testing the factorial method") {
   CHECK(factorial(0) == 1);
   CHECK(factorial(1) == 1);
   CHECK(factorial(2) == 2);
   CHECK(factorial(3) == 6);
   CHECK(factorial(10) == 3628800);
+}
+
+TEST_CASE("Testing the library") {
+  hello();
 }
