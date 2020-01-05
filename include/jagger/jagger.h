2abfc8568c3db87d6080d333be0a911189c4a984
Mauro Giacomini <mauro.giaco@gmail.com>
2020-01-05 10:13:08 +0100

    Refactor flags into bitwise

--- include/jagger/jagger.h
+++ include/jagger/jagger.h
@@ -1,3 +1,21 @@
 #pragma once
 
-void hello();
+#ifndef JAGGER_H
+#define JAGGER_H
+
+#define JAGGER_VERSION "0.1.0"
+
+// Constants; mode and level
+#define LOG_MODE_OFF      0x00000000
+#define LOG_MODE_CONSOLE  0x00000001
+#define LOG_MODE_FILE     0x00000010
+#define LOG_LEVEL_TRACE   0x00000001
+#define LOG_LEVEL_DEBUG   0x00000010
+#define LOG_LEVEL_INFO    0x00000100
+#define LOG_LEVEL_WARNING 0x00001000
+#define LOG_LEVEL_ERROR   0x00010000
+#define LOG_LEVEL_FATAL   0x00100000
+
+int jagger_init(const unsigned int mode, const unsigned int level, const char *log_file);
+
+#endif
--- src/jagger.cpp
+++ src/jagger.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <jagger/jagger.h>
 
-void hello() {
-  printf("Hello World!\n");
-  printf("From JAGGER!\n");
+int jagger_init(const unsigned int mode, const unsigned int level, const char *log_file) {
+  // printf("mode    : %d\n", mode);
+  // printf("level   : %d\n", level);
+  // printf("log_file: %s\n", log_file);
+  if ((mode & LOG_MODE_FILE) && log_file == NULL) {
+    // perror("No filename specified!");
+    return 0;
+  }
+
+  return -1;
 }
